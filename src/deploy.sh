c9db9b586ecf3f7d570f46184c0eaaf8ec1664ce
prunesga <mihruz@gmail.com>
2024-02-04 14:15:48 +0300

    5-1

---
+++ src/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp ./src/cat/s21_cat prunesga@192.168.100.2:/usr/local/bin/
+scp ./src/grep/s21_grep prunesga@192.168.100.2:/usr/local/bin/
+ssh prunesga@192.168.100.2 ls /usr/local/bin
